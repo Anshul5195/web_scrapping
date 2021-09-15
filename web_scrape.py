afd2a5b14ee1bd631d57da6160ebf934421713d5
Anshul Rajput <anshulrajput@Anshuls-MacBook-Air.local>
2021-09-15 19:03:01 +0530

    Restructure

--- web_scrape.py
+++ web_scrape.py
@@ -11,10 +11,7 @@ with open('sample.csv', 'w') as csv_file:
 
     for article in soup.find_all('article'):
         headline = article.h2.a.text
-        print(headline)
-
         summary = article.find('div', class_="entry-content").p.text
-        print(summary)
 
         try:
             vid_src = article.find('iframe', class_="youtube-player")['src']
@@ -23,9 +20,6 @@ with open('sample.csv', 'w') as csv_file:
 
         except Exception as e:
             yt_link = None
-
-        print(yt_link)
-        print()
 
         csv_writer.writerow([headline, summary, yt_link])
 
