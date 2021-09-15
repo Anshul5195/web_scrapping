dc2be8f55923da22e8d4ad166f2a032771631b41
Anshul Rajput <anshulrajput@Anshuls-MacBook-Air.local>
2021-09-15 18:59:09 +0530

    Added data into csv file

--- web_scrape.py
+++ web_scrape.py
@@ -5,20 +5,29 @@ import csv
 source = requests.get('http://coreyms.com').text
 soup = BeautifulSoup(source, 'lxml')
 
-for article in soup.find_all('article'):
-    headline = article.h2.a.text
-    print(headline)
+with open('sample.csv', 'w') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['headline', 'summary', 'video_link'])
 
-    summary = article.find('div', class_="entry-content").p.text
-    print(summary)
+    for article in soup.find_all('article'):
+        headline = article.h2.a.text
+        print(headline)
 
-    try:
-        vid_src = article.find('iframe', class_="youtube-player")['src']
-        vid_id = vid_src.split('/')[4].split('?')[0]
-        yt_link = f'https://youtube.com/watch?v={vid_id}'
+        summary = article.find('div', class_="entry-content").p.text
+        print(summary)
 
-    except Exception as e:
-        yt_link = None
+        try:
+            vid_src = article.find('iframe', class_="youtube-player")['src']
+            vid_id = vid_src.split('/')[4].split('?')[0]
+            yt_link = f'https://youtube.com/watch?v={vid_id}'
 
-    print(yt_link)
-    print()
+        except Exception as e:
+            yt_link = None
+
+        print(yt_link)
+        print()
+
+        csv_writer.writerow([headline, summary, yt_link])
+
+
+
